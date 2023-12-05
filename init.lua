41c3db11a094de9f9da72bef0f7e8263c23572e0
David Robertsson <david.robertsson@gmail.com>
2023-12-05 20:35:45 +0100

    Neovim dotfile
    To be expanded and tweaked

---
+++ init.lua
@@ -0,0 +1,366 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+local fmt = string.format
+
+local pack_path = fn.stdpath("data") .. "/site/pack"
+
+-- ensure a given plugin from github.com/<user>/<repo> is cloned in the pack/packer/start directory
+local function ensure (user, repo)
+  local install_path = fmt("%s/packer/start/%s", pack_path, repo)
+  if fn.empty(fn.glob(install_path)) > 0 then
+    execute(fmt("!git clone https://github.com/%s/%s %s", user, repo, install_path))
+    execute(fmt("packadd %s", repo))
+  end
+end
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+{
+  'projekt0n/github-nvim-theme',
+  config = function()
+    vim.cmd('colorscheme github_dark')
+  end},
+{'nvim-treesitter/nvim-treesitter', run =':TSUpdate'},
+{'hrsh7th/cmp-vsnip'},
+{'wbthomason/packer.nvim'},
+{'neovim/nvim-lspconfig'},
+{'hrsh7th/nvim-cmp'},
+{'hrsh7th/cmp-nvim-lsp'},
+{'hrsh7th/cmp-buffer'},
+{'hrsh7th/cmp-path'},
+{'hrsh7th/cmp-cmdline'},
+{'hrsh7th/vim-vsnip'},
+{
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",         -- required
+    "nvim-telescope/telescope.nvim", -- optional
+    "sindrets/diffview.nvim",        -- optional
+    "ibhagwan/fzf-lua",              -- optional
+  },
+  config = true
+}
+})
+
+-- `on_attach` callback will be called after a language server
+-- instance has been attached to an open buffer with matching filetype
+-- here we're setting key mappings for hover documentation, goto definitions, goto references, etc
+-- you may set those key mappings based on your own preference
+local on_attach = function(client, bufnr)
+  local opts = { noremap=true, silent=true }
+
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cr', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>cd', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', 'd', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+end
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- setting up the elixir language server
+-- you have to manually specify the entrypoint cmd for elixir-ls
+require'lspconfig'.elixirls.setup{
+    cmd = { "/home/drobban/elixir-ls/language_server.sh" },
+    on_attach = on_attach,
+    capabilities = capabilities
+}
+
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      -- setting up snippet engine
+      -- this is for vsnip, if you're using other
+      -- snippet engine, please refer to the `nvim-cmp` guide
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, -- For vsnip users.
+    { name = 'buffer' }
+  }),
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    -- other mappings ...
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+	cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+	feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+	cmp.complete()
+      else
+	fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+	cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+	feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" })
+  }
+})
+
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {"elixir", "heex", "eex"}, -- only install parsers for elixir and heex
+  -- ensure_installed = "all", -- install parsers for all supported languages
+  sync_install = false,
+  ignore_install = { },
+  highlight = {
+    enable = true,
+    disable = { },
+  },
+}
+
+
+-- init.lua
+local neogit = require("neogit")
+
+neogit.setup {
+  -- Hides the hints at the top of the status buffer
+  disable_hint = false,
+  -- Disables changing the buffer highlights based on where the cursor is.
+  disable_context_highlighting = false,
+  -- Disables signs for sections/items/hunks
+  disable_signs = false,
+  -- Do not ask to confirm the commit - just do it when the buffer is closed.
+  disable_commit_confirmation = false,
+  -- Changes what mode the Commit Editor starts in. `true` will leave nvim in normal mode, `false` will change nvim to
+  -- insert mode, and `"auto"` will change nvim to insert mode IF the commit message is empty, otherwise leaving it in
+  -- normal mode.
+  disable_insert_on_commit = true,
+  -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
+  -- events.
+  filewatcher = {
+    interval = 1000,
+    enabled = true,
+  },
+  -- "ascii"   is the graph the git CLI generates
+  -- "unicode" is the graph like https://github.com/rbong/vim-flog
+  graph_style = "ascii", 
+  -- Used to generate URL's for branch popup action "pull request".
+  git_services = {
+    ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+    ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+    ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+  },
+  -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
+  -- sorter instead. By default, this function returns `nil`.
+  telescope_sorter = function()
+    return require("telescope").extensions.fzf.native_fzf_sorter()
+  end,
+  -- Persist the values of switches/options within and across sessions
+  remember_settings = true,
+  -- Scope persisted settings on a per-project basis
+  use_per_project_settings = true,
+  -- Table of settings to never persist. Uses format "Filetype--cli-value"
+  ignored_settings = {
+    "NeogitPushPopup--force-with-lease",
+    "NeogitPushPopup--force",
+    "NeogitPullPopup--rebase",
+    "NeogitCommitPopup--allow-empty",
+    "NeogitRevertPopup--no-edit",
+  },
+  -- Set to false if you want to be responsible for creating _ALL_ keymappings
+  use_default_keymaps = true,
+  -- Neogit refreshes its internal state after specific events, which can be expensive depending on the repository size.
+  -- Disabling `auto_refresh` will make it so you have to manually refresh the status after you open it.
+  auto_refresh = true,
+  -- Value used for `--sort` option for `git branch` command
+  -- By default, branches will be sorted by commit date descending
+  -- Flag description: https://git-scm.com/docs/git-branch#Documentation/git-branch.txt---sortltkeygt
+  -- Sorting keys: https://git-scm.com/docs/git-for-each-ref#_options
+  sort_branches = "-committerdate",
+  -- Change the default way of opening neogit
+  kind = "tab",
+  -- Disable line numbers and relative line numbers
+  disable_line_numbers = true,
+  -- The time after which an output console is shown for slow running commands
+  console_timeout = 2000,
+  -- Automatically show console if a command takes more than console_timeout milliseconds
+  auto_show_console = true,
+  status = {
+    recent_commit_count = 10,
+  },
+  commit_editor = {
+    kind = "auto",
+  },
+  commit_select_view = {
+    kind = "tab",
+  },
+  commit_view = {
+    kind = "vsplit",
+    verify_commit = os.execute("which gpg") == 0, -- Can be set to true or false, otherwise we try to find the binary
+  },
+  log_view = {
+    kind = "tab",
+  },
+  rebase_editor = {
+    kind = "auto",
+  },
+  reflog_view = {
+    kind = "tab",
+  },
+  merge_editor = {
+    kind = "auto",
+  },
+  tag_editor = {
+    kind = "auto",
+  },
+  preview_buffer = {
+    kind = "split",
+  },
+  popup = {
+    kind = "split",
+  },
+  signs = {
+    -- { CLOSED, OPENED }
+    hunk = { "", "" },
+    item = { ">", "v" },
+    section = { ">", "v" },
+  },
+  -- Each Integration is auto-detected through plugin presence, however, it can be disabled by setting to `false`
+  integrations = {
+    -- If enabled, use telescope for menu selection rather than vim.ui.select.
+    -- Allows multi-select and some things that vim.ui.select doesn't.
+    telescope = nil,
+    -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
+    -- The diffview integration enables the diff popup.
+    --
+    -- Requires you to have `sindrets/diffview.nvim` installed.
+    diffview = nil,
+
+    -- If enabled, uses fzf-lua for menu selection. If the telescope integration
+    -- is also selected then telescope is used instead
+    -- Requires you to have `ibhagwan/fzf-lua` installed.
+    fzf_lua = nil,
+  },
+  sections = {
+    -- Reverting/Cherry Picking
+    sequencer = {
+      folded = false,
+      hidden = false,
+    },
+    untracked = {
+      folded = false,
+      hidden = false,
+    },
+    unstaged = {
+      folded = false,
+      hidden = false,
+    },
+    staged = {
+      folded = false,
+      hidden = false,
+    },
+    stashes = {
+      folded = true,
+      hidden = false,
+    },
+    unpulled_upstream = {
+      folded = true,
+      hidden = false,
+    },
+    unmerged_upstream = {
+      folded = false,
+      hidden = false,
+    },
+    unpulled_pushRemote = {
+      folded = true,
+      hidden = false,
+    },
+    unmerged_pushRemote = {
+      folded = false,
+      hidden = false,
+    },
+    recent = {
+      folded = true,
+      hidden = false,
+    },
+    rebase = {
+      folded = true,
+      hidden = false,
+    },
+  },
+  mappings = {
+    finder = {
+      ["<cr>"] = "Select",
+      ["<c-c>"] = "Close",
+      ["<esc>"] = "Close",
+      ["<c-n>"] = "Next",
+      ["<c-p>"] = "Previous",
+      ["<down>"] = "Next",
+      ["<up>"] = "Previous",
+      ["<tab>"] = "MultiselectToggleNext",
+      ["<s-tab>"] = "MultiselectTogglePrevious",
+      ["<c-j>"] = "NOP",
+    },
+    -- Setting any of these to `false` will disable the mapping.
+    popup = {
+      ["?"] = "HelpPopup",
+      ["A"] = "CherryPickPopup",
+      ["D"] = "DiffPopup",
+      ["M"] = "RemotePopup",
+      ["P"] = "PushPopup",
+      ["X"] = "ResetPopup",
+      ["Z"] = "StashPopup",
+      ["b"] = "BranchPopup",
+      ["c"] = "CommitPopup",
+      ["f"] = "FetchPopup",
+      ["l"] = "LogPopup",
+      ["m"] = "MergePopup",
+      ["p"] = "PullPopup",
+      ["r"] = "RebasePopup",
+      ["v"] = "RevertPopup",
+    },
+    status = {
+      ["q"] = "Close",
+      ["I"] = "InitRepo",
+      ["1"] = "Depth1",
+      ["2"] = "Depth2",
+      ["3"] = "Depth3",
+      ["4"] = "Depth4",
+      ["<tab>"] = "Toggle",
+      ["x"] = "Discard",
+      ["s"] = "Stage",
+      ["S"] = "StageUnstaged",
+      ["<c-s>"] = "StageAll",
+      ["u"] = "Unstage",
+      ["U"] = "UnstageStaged",
+      ["d"] = "DiffAtFile",
+      ["$"] = "CommandHistory",
+      ["#"] = "Console",
+      ["Y"] = "YankSelected",
+      ["<c-r>"] = "RefreshBuffer",
+      ["<enter>"] = "GoToFile",
+      ["<c-v>"] = "VSplitOpen",
+      ["<c-x>"] = "SplitOpen",
+      ["<c-t>"] = "TabOpen",
+      ["{"] = "GoToPreviousHunkHeader",
+      ["}"] = "GoToNextHunkHeader",
+    },
+  },
+}
+
