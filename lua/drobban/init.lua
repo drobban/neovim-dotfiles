2cd49f0581cbf10a0048fd8b94b8611525e7ea15
David Robertsson <david.robertsson@gmail.com>
2023-12-07 19:29:38 +0100

    Tweaked keymap for closing buffers now does it without killing the window as well
    kill buffer and window on IEx end.

--- lua/drobban/init.lua
+++ lua/drobban/init.lua
@@ -4,3 +4,6 @@ vim.api.nvim_set_option("clipboard","unnamedplus")
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "number"
+
+-- Close IEx term on end...
+vim.g.ConqueTerm_CloseOnEnd = 1
--- lua/drobban/remap.lua
+++ lua/drobban/remap.lua
@@ -5,7 +5,9 @@ vim.keymap.set("n", "<leader>qq", vim.cmd.q)
 vim.keymap.set("n", "<leader>wh", vim.cmd.vsplit)
 vim.keymap.set("n", "<leader>wv", vim.cmd.split)
 vim.keymap.set("n", "<leader>wd", vim.cmd.close)
-vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+-- vim.keymap.set("n", "<leader>bd", vim.cmd.bd)
+-- vim.keymap.set("n", "<leader>bd", ':bp<bar>sp<bar>bn<bar>bd<CR>')
+vim.keymap.set("n", "<leader>bd", ':bp|sp|bn|bd<CR>')
 
 -- Move text chunks in visual mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -17,4 +19,9 @@ vim.keymap.set("n", "J", "mzJ`z")
 -- gets rid of the enoying paste replacement - when pasting over visual
 vim.keymap.set("x", "p", "\"_dP")
 
-
+-- Window jump mapping
+for i = 1, 6 do
+  local lhs = "<leader>" .. i
+  local rhs = i .. "<c-w>w"
+  vim.keymap.set("n", lhs, rhs, { desc = "Move to window " .. i })
+end
